7a449fa0d1b739d9ffd12f416dfa1ebd5b906850
Ankit Podder <ankitpodder0211@gmail.com>
2020-10-21 12:22:26 +0530

    Updates

---
+++ precache-manifest.cce7f360efa9612c927d6e5475eda670.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "890b0f2891fd9edc8ba7945717164b84",
+    "url": "/dadjokes/index.html"
+  },
+  {
+    "revision": "5a205454d282d4be3183",
+    "url": "/dadjokes/static/css/main.2746080f.chunk.css"
+  },
+  {
+    "revision": "7a9698b9ef77e4a5733d",
+    "url": "/dadjokes/static/js/2.01bda1a1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/dadjokes/static/js/2.01bda1a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a205454d282d4be3183",
+    "url": "/dadjokes/static/js/main.22cf91e4.chunk.js"
+  },
+  {
+    "revision": "4fc60c61b5b66753f888",
+    "url": "/dadjokes/static/js/runtime-main.2ba0c6ad.js"
+  }
+]);
