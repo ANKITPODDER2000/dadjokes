de085fb0a1f12eeff45034de162d2d54b5965b31
Ankit Podder <ankitpodder0211@gmail.com>
2020-10-21 12:17:52 +0530

    Ready to deploy

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Jokegen from "./Jokegen";
+import './App.css';
+
+class App extends Component{
+    render() {
+        return (
+            <div className="App">
+                <Jokegen />
+            </div>
+        )
+    }
+}
+
+export default App;
