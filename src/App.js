651f162f989ccc8370c539753474bdce651437de
Ankit Podder <ankitpodder0211@gmail.com>
2020-11-12 01:44:46 +0530

    add docs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+//Import main component
 import Jokegen from "./Jokegen";
+//Import app.css for styling
 import './App.css';
 
 class App extends Component{
